4249de2dd23a18f4ce79cb709ed313c33f155c53
613dydy <youpbit@gmail.com>
2025-07-14 20:11:36 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcvZw3O1P8mW-__Xw7tYXfLwEuaTy86h8",
@@ -13,20 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
 
-document.getElementById("loginBtn").onclick = async () => {
-  const provider = new GoogleAuthProvider();
-  await signInWithPopup(auth, provider);
-};
-
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    document.getElementById("main").style.display = "block";
-    window.currentUser = user;
-    window.db = db;
-    window.uid = user.uid;
-    loadVideos();
-  }
-});
+window.db = db;
